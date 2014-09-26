06dd52e08df30f8320173582aa99d5fcbdac8a65
Dimitris Kontokostas <jimkont@gmail.com>
2014-09-26 08:40:12 +0300

    doc

--- src/main/java/org/dbpedia/extraction/live/mirror/sparul/SPARULExecutor.java
+++ src/main/java/org/dbpedia/extraction/live/mirror/sparul/SPARULExecutor.java
@@ -1,11 +1,20 @@
 package org.dbpedia.extraction.live.mirror.sparul;
 
 /**
- * Description
+ * Interface to abstract different triple-store quirks
  *
  * @author Dimitris Kontokostas
- * @since 9/24/14 11:12 AM
+ * @since 9 /24/14 11:12 AM
  */
 public interface SPARULExecutor {
+
+    /**
+     * Executes a SPARUL Query
+     *
+     * @param sparulQuery the sparul query
+     * @throws SPARULException the sPARUL exception
+     */
     void executeSPARUL(String sparulQuery) throws SPARULException;
+
+
 }
